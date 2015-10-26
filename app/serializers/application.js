83bf316b6da51679333edd63b6fe2455558f6bca
Linus Oleander <linus@oleander.nu>
2015-10-26 16:02:36 +0100

    Embed posts

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -3,7 +3,7 @@ import LFSerializer from 'ember-localforage-adapter/serializers/localforage';
 export default LFSerializer.extend(
   DS.EmbeddedRecordsMixin, {
     attrs: {
-      magnets: { embedded: "always" }
+      posts: { embedded: "always" }
     }
   }
 );
